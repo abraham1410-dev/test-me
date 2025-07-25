b5acc9a9f8f7c922d2ce82650cc30c014d4a6ec4
Abiraham Immanvel Aasakoilraj <abirahamimmanvel.aasakoilraj@aroopatech.com>
2025-07-25 11:15:33 +0530

    inital code push

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AggridExample from "./pages/AggridExample";
+
+function App() {
+  return (
+    <div className="w-dvw h-dvh">
+      <div className="w-full h-full p-10">
+        <AggridExample />
+      </div>
+    </div>
+  );
+}
+
+export default App;
